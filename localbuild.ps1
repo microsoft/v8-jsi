09a7700a08a964afdadbac5549bb414c8df9a1bc
tudorms <48035227+tudorms@users.noreply.github.com>
2020-06-08 17:24:00 -0700

    Fix basic debugging support. Upgrade to V8 8.4 (#15)
    * Fix a couple of bugs around the inspector (crash on detach, hang on shutdown with debugger attached, etc.);
    * Upgrade to V8 version 8.4;

--- localbuild.ps1
+++ localbuild.ps1
@@ -2,28 +2,31 @@
 # Licensed under the MIT license.
 param(
     [string]$Platform = "x64", #"arm64",
-    [string]$Configuration = "Debug" # "UWP-Release" "Release-Clang"
+    [string]$Configuration = "Debug", # "UWP-Release" "Release-Clang"
+    [bool]$Setup = $true
 )
 
 $OutputPath = "$PSScriptRoot\out"
 $SourcesPath = $PSScriptRoot
-$Platforms = "x64", "x86", "arm64"
-$Configurations = "Debug", "Release", "Release-Clang", "EXPERIMENTAL-libcpp-Clang"
+$Platforms = "x64", "x86"#, "arm64"
+$Configurations = "Debug", "Release", "UWP-Release"#, "Release-Clang", "EXPERIMENTAL-libcpp-Clang"
 
-Write-Host "Downloading environment..."
-& ".\scripts\download_depottools.ps1" -SourcesPath $SourcesPath
+if ($Setup) {
+    Write-Host "Downloading environment..."
+    & ".\scripts\download_depottools.ps1" -SourcesPath $SourcesPath
 
-if (!$?) {
-    Write-Host "Failed to download depot-tools"
-    exit 1
-}
+    if (!$?) {
+        Write-Host "Failed to download depot-tools"
+        exit 1
+    }
 
-Write-Host "Fetching code..."
-& ".\scripts\fetch_code.ps1" -SourcesPath $SourcesPath -OutputPath $OutputPath -Configuration $Configuration
+    Write-Host "Fetching code..."
+    & ".\scripts\fetch_code.ps1" -SourcesPath $SourcesPath -OutputPath $OutputPath -Configuration $Configuration
 
-if (!$?) {
-    Write-Host "Failed to retrieve the v8 code"
-    exit 1
+    if (!$?) {
+        Write-Host "Failed to retrieve the v8 code"
+        exit 1
+    }
 }
 
 if ($Platform -like "all") {
