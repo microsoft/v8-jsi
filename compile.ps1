288746b8f61a02ff0f7eb646b55001c4df748b1b
Julio C. Rocha <julio.rocha@microsoft.com>
2021-07-20 23:52:00 -0700

    Removed compile.ps1

--- compile.ps1
+++
@@ -1,7 +0,0 @@
-& .\localbuild.ps1 -NoSetup
-if (!$?) {
-    Write-Host "Build failure, check logs for details"
-    exit 1
-}
-
-e:\GitHub\vmoroz\v8-jsi\build\v8build\v8\out\win32\x64\Debug\jsitests.exe
