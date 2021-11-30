cda910d805777ce30160dd7e3d4847fca2737244
tudorms <tudor.mihai@microsoft.com>
2021-11-30 09:57:34 -0800

    Fix lock, add unit test

--- src/inspector/inspector_socket_server.cpp
+++ src/inspector/inspector_socket_server.cpp
@@ -339,12 +339,16 @@ bool InspectorSocketServer::HasTargets() {
 bool InspectorSocketServer::Start() {
   state_ = ServerState::kRunning;
 
-  // It feels safer to lock before the thread jump; if we get through Start, then Stop, then the thread gets scheduled now touching deleted memory that's not good either.
-  // In real-life this won't really matter; it's only an issue in unit tests that quickly bring up and tear down runtimes.
-  std::unique_lock<std::mutex> tcp_server_lock(mutex_tcp_server_);
-  std::thread([this, lock = std::move(tcp_server_lock)]() {
-    tcp_server_ = std::make_shared<tcp_server>(
-        port_, InspectorSocketServer::SocketConnectedCallback, this);
+  std::thread([this]() {
+    {
+      std::unique_lock<std::mutex> tcp_server_lock(mutex_tcp_server_);
+      if (tcp_server_stopped_) {
+        return;
+      }
+
+      tcp_server_ = std::make_shared<tcp_server>(port_, InspectorSocketServer::SocketConnectedCallback, this);
+    }
+
     tcp_server_->run();
   }).detach();
   return true;
@@ -361,6 +365,7 @@ void InspectorSocketServer::Stop() {
     std::unique_lock<std::mutex> tcp_server_lock(mutex_tcp_server_);
     // This will stop the the server io_context which will result in stopping the server thread as well.
     tcp_server_->stop();
+    tcp_server_stopped_ = true;
   }
 
   if (state_ == ServerState::kStopped) {
