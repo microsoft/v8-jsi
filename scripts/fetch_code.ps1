8c48ae6765f79d730bfb6fd0043ff0f823446895
Yichen Yao <yicyao@microsoft.com>
2022-11-04 11:15:11 -0700

    Remove v8build\v8\third_party\depot_tools\external_bin\gsutil to avoid cg alerts.

--- scripts/fetch_code.ps1
+++ scripts/fetch_code.ps1
@@ -89,6 +89,7 @@ if ($AppPlatform -eq "linux") {
 
 # Remove unused code
 Remove-Item -Recurse -Force (Join-Path $workpath "v8build\v8\test\test262\data\tools")
+Remove-Item -Recurse -Force (Join-Path $workpath "v8build\v8\third_party\depot_tools\external_bin\gsutil")
 Remove-Item -Recurse -Force (Join-Path $workpath "v8build\v8\third_party\google_benchmark")
 Remove-Item -Recurse -Force (Join-Path $workpath "v8build\v8\third_party\perfetto")
 Remove-Item -Recurse -Force (Join-Path $workpath "v8build\v8\third_party\protobuf")
