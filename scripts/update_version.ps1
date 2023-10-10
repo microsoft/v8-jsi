7423f567453e7ca7f93bbc0e210205f259b2ad02
tudorms <tudor.mihai@microsoft.com>
2023-10-10 12:00:38 -0700

    Update script

--- scripts/update_version.ps1
+++ scripts/update_version.ps1
@@ -4,6 +4,10 @@ param(
     [System.IO.DirectoryInfo]$SourcesPath = $PSScriptRoot
 )
 
+# Details about the V8 release process: https://v8.dev/docs/release-process
+
+# TODO: a cron ADO task should trigger this script to update the version
+
 # https://omahaproxy.appspot.com is deprecated in favor of https://chromiumdash.appspot.com
 $latestVersion = (Invoke-WebRequest "https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows&num=1" -UseBasicParsing | ConvertFrom-Json).milestone / 10
 
@@ -29,11 +33,36 @@ function GetLatestBuildNumberForVersion($buildingVersion) {
   $minorVersion = (($content | Select-String -Pattern "V8_MINOR_VERSION (\d+)").Matches[0].Groups[1].Value).Trim()
   $buildNumber = (($content | Select-String -Pattern "V8_BUILD_NUMBER (\d+)").Matches[0].Groups[1].Value).Trim()
   $patchLevel = (($content | Select-String -Pattern "V8_PATCH_LEVEL (\d+)").Matches[0].Groups[1].Value).Trim()
-  return "$buildNumber.$patchLevel"
+
+  # TODO: do this for every new $patchLevel as well?
+  return $buildNumber
+}
+
+function BumpSemVer($version) {
+  $versionParts = $version.Split('.')
+  $versionParts[2] = [int]$versionParts[2] + 1
+  return $versionParts -join '.'
 }
 
 $buildNumber = GetLatestBuildNumberForVersion($latestVersion)
 
 Write-Host "Latest build number upstream is $buildNumber"
+Write-Host "Build number currently being built is $($config.buildNumber)"
 
-# TODO: continue implementation here (config.json should hold buildNumber.patchLevel as well, and a cron ADO task should trigger this script to update the version)
+if ($buildNumber -eq $config.buildNumber) {
+  Write-Host "Latest build number is already being built. All good!"
+  exit 0
+}
+
+Write-Host "New stable build number released, attempting to bump it"
+
+$config.buildNumber = $buildNumber
+$config.version = BumpSemVer($config.version)
+
+ConvertTo-Json -InputObject $config | Set-Content (Join-Path $SourcesPath "config.json")
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add config.json
+git commit -m "Updating version to $($config.version) (new upstream build number $buildNumber)"
+git push
