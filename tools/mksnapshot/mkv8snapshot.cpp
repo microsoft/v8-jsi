6c0f70bdfd9fb4386b4acfa0e5af77db4383f268
Vladimir Morozov <vmorozov@microsoft.com>
2026-07-28 22:47:49 -0700

    Fix deadlock in acceptance testing (#254)

--- tools/mksnapshot/mkv8snapshot.cpp
+++ tools/mksnapshot/mkv8snapshot.cpp
@@ -180,5 +180,8 @@ int main(int argc, char** argv) {
   }
 
   std::fprintf(stderr, "[mkv8snapshot] wrote %s (%zu bytes)\n", outPath.c_str(), blobSize);
-  return 0;
+  std::fflush(stderr);
+  // V8 engine background threads can deadlock during DLL/process teardown; blob is on disk.
+  ::TerminateProcess(::GetCurrentProcess(), 0);
+  return 0;  // unreachable
 }
